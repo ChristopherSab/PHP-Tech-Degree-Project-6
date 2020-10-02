f58e960d8b45e90c0b372213b7f121b2106a5512
ChristopherSab <chris.sabaoth@protonmail.com>
2020-10-02 22:01:15 +0100

    Added tests for exception in ListingBasic class

--- tests/ListingBasicTest.php
+++ tests/ListingBasicTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ .'/../classes/ListingBasic.php';
+require_once __DIR__ .'/../classes/ListingBasic.php';
 
 use PHPUnit\Framework\TestCase;
 
@@ -10,8 +10,17 @@ class ListingBasicTest extends TestCase
     /** @test */
     function listingBasicMustReceiveData()
     {
-        $this->expectExeption(InvalidArgumentException::class);
+        $this->expectException(Exception::class);
         $listingBasic = new ListingBasic($data = []);
     }
+
+    /** @test */
+    function needsValidIdToCreateAListing()
+    {
+        $this->expectException(Exception::class);
+        $listingBasic = new ListingBasic();
+        $listingBasic->setValues(); 
+    }
+
 
 }
